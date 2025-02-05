d96b3b43d8b9a558d2382637f03853d1c2f99a8e
Noah Saso <noahsaso@gmail.com>
2025-02-05 08:46:20 -0500

    improved formatting and logging

--- bot.py
+++ bot.py
@@ -12,19 +12,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    notify_channel = await client.fetch_channel(NOTIFY_CHANNEL_ID)
+
     content = message.content.lower()
     if "axvdex.org" in content or ("astrovault" in content and "airdrop" in content):
-        print(f"Deleting message from {message.author.display_name}")
-        print(f"  > {message.content}")
+        print(f"Deleting message from {message.author.display_name}:")
+        print("---------------------------------------")
+        print(message.content)
+        print("---------------------------------------")
 
-        # delete message
-        await message.delete()
+        try:
+            # delete message
+            await message.delete()
 
-        # time out user
-        await message.author.timeout(duration=timedelta(days=27), reason="spam")
+            # time out user
+            await message.author.timeout(duration=timedelta(days=27), reason="spam")
 
-        # notify of timeout
-        notify_channel = await client.fetch_channel(NOTIFY_CHANNEL_ID)
-        await notify_channel.send(f"{message.author.mention} timed out for spam")
+            # notify of timeout
+            await notify_channel.send(f"{message.author.mention} timed out for spam")
+        except Exception as e:
+            print(f"Error: {e}")
+            await notify_channel.send(f"Error deleting spam message or timing out user ({message.author.mention}):\n```{e}```")
 
 client.run(TOKEN)
